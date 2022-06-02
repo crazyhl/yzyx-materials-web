43a96bdb1c50053f1772110994bd13b0304d49ef
M1racle.Hao <crazyhl@163.com>
2022-06-02 10:47:01 +0800

    feat add dockerfile

--- src/boot/axios.ts
+++ src/boot/axios.ts
@@ -15,7 +15,9 @@ declare module '@vue/runtime-core' {
 // good idea to move this instance creation inside of the
 // "export default () => {}" function below (which runs individually
 // for each client)
-const api = axios.create({ baseURL: 'http://127.0.0.1:8080' })
+
+const baseUrl = process.env.NODE_ENV === 'production' ? 'https://backend:8080' : 'http://127.0.0.1:8080'
+const api = axios.create({ baseURL: baseUrl })
 const userStore = useUserStore()
 
 // 添加请求拦截器
