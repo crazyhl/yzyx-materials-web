db0c0c3d8d1c51526c14949b8981d4b3b4dc574a
M1racle.Hao <crazyhl@163.com>
2022-07-14 10:44:25 +0800

    feat 账户详情支持已绑定品种显示

--- src/pages/AccountDetailPage.vue
+++ src/pages/AccountDetailPage.vue
@@ -24,7 +24,59 @@
         </q-btn>
       </q-card-actions>
     </q-card>
-    下面就是品种列表了
+    <div class="q-mt-md">
+      <q-table
+        title="已绑定品种列表"
+        :rows="breedListData"
+        :columns="columns"
+        row-key="name"
+        :rows-per-page-options="[10]"
+      >
+      <template v-slot:body-cell-value="props">
+        <q-td :props="props">
+          <div>
+            <q-badge v-if="props.value.cost > props.value.net_value" color="green">
+            {{props.value.cost}}/{{props.value.net_value}}
+            </q-badge>
+            <q-badge v-else color="red">
+            {{props.value.cost}}/{{props.value.net_value}}
+            </q-badge>
+          </div>
+        </q-td>
+      </template>
+      <template v-slot:body-cell-total_value="props">
+        <q-td :props="props">
+          <div>
+            <q-badge v-if="props.value.total_cost > props.value.total_net_value" color="green">
+            {{props.value.total_cost}}/{{props.value.total_net_value}}
+            </q-badge>
+            <q-badge v-else color="red">
+            {{props.value.total_cost}}/{{props.value.total_net_value}}
+            </q-badge>
+          </div>
+        </q-td>
+      </template>
+      <template v-slot:body-cell-profit="props">
+        <q-td :props="props">
+          <div>
+            <q-badge v-if="props.value.total_cost > props.value.total_net_value" color="green">
+            {{props.value.total_cost - props.value.total_net_value}}
+            </q-badge>
+            <q-badge v-else color="red">
+            {{props.value.total_cost - props.value.total_net_value}}
+            </q-badge>
+          </div>
+        </q-td>
+      </template>
+      <template v-slot:body-cell-action="props">
+        <q-td :props="props">
+          <div>
+            记一笔位置
+          </div>
+        </q-td>
+      </template>
+      </q-table>
+    </div>
   </div>
   <edit-account-dialog :show-edit-account-dialog="showEditAccountDialog" @close-dialog="showEditAccountDialog = false" @edit-success="editSuccess" :account="account" />
   <account-bind-breed-dialog :show-dialog="showAccountBindBreedDialog" :account="account" @close-dialog="showAccountBindBreedDialog = false" @bind-success="bindSuccess" />
@@ -36,6 +88,7 @@ import { useRoute } from 'vue-router'
 import { Account, detailAccount } from '../api/account'
 import EditAccountDialog from 'src/components/EditAccountDialog.vue'
 import AccountBindBreedDialog from 'src/components/AccountBindBreedDialog.vue'
+import { Breed } from 'src/api/breed'
 
 const formatFields = (account: Account) => {
   accountCreateTimeStr.value = dayjs.unix(account.create_at).format('YYYY-MM-DD')
@@ -68,6 +121,9 @@ detailAccount(parseInt(route.params.id.toString()))
     account.value = data.data
     console.log(account.value)
     formatFields(account.value)
+    if (data.data.breeds) {
+      breedListData.value = data.data.breeds
+    }
   })
 
 const showEditAccountDialog = ref(false)
@@ -84,5 +140,38 @@ const editSuccess = (acc: Account) => {
 const bindSuccess = () => {
   console.log('aaaaaa')
 }
+
+const columns = [
+  { name: 'code', label: '代码', field: 'code', sortable: false },
+  { name: 'name', label: '名称', field: 'name', format: (val: string) => `${val}`, sortable: false },
+  {
+    name: 'value',
+    label: '成本/净值',
+    field: (row:Breed) => row,
+    sortable: false
+  },
+  { name: 'total_count', label: '总份数', field: 'total_count', sortable: false },
+  { name: 'profit', label: '利润', field: (row:Breed) => row, sortable: false },
+  { name: 'total_value', label: '总成本/总净值', field: (row:Breed) => row, sortable: false },
+  {
+    name: 'time',
+    label: '添加/更新时间',
+    field: (row:Breed) => row,
+    format: (val: Breed) => {
+      const createTimeStr = dayjs.unix(val.create_at).format('YYYY-MM-DD')
+      const updateTimeStr = dayjs.unix(val.update_at).format('YYYY-MM-DD')
+      return createTimeStr + '/' + updateTimeStr
+    },
+    sortable: false
+  },
+  {
+    name: 'action',
+    label: '操作',
+    field: (row: Breed) => row,
+    sortable: false
+  }
+]
+
+const breedListData = ref<Breed[]>([])
 
 </script>
